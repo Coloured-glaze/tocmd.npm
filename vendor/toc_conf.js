bc7c0284995fc21c66a432e4f4928da60cc95338
Coloured-glaze <1044187534@qq.com>
2022-03-03 15:23:49 +0800

    添加注释

--- vendor/toc_conf.js
+++ vendor/toc_conf.js
@@ -3,7 +3,7 @@ var jquery_ztree_toc_opts = {
     is_auto_number:true,
     documment_selector:'.markdown-body',
     ztreeStyle: {
-        width:'326px',
+        width:'25%',    //左侧列表滑块显示位置 326px or 25%
         overflow: 'auto',
         position: 'fixed',
         'z-index': 2147483647,
@@ -15,6 +15,6 @@ var jquery_ztree_toc_opts = {
     }
 }
 var markdown_panel_style = {
-    'width':'70%',
-    'margin-left':'25%'
+    'width':'70%',  //右侧文本显示框占用宽度 326px or 70%
+    'margin-left':'326px' //左侧列表显示框占用宽度 326px or 25%
 };
